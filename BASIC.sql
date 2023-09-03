85e69de4ee27812886e395fa051f6e6eb40078fd
9694manish <133850760+9694manish@users.noreply.github.com>
2023-09-03 19:53:03 +0530

    Basic Syntax

---
+++ BASIC.sql
@@ -0,0 +1,70 @@
+
+
+show databases;
+
+
+use sakila;
+
+show tables;
+
+-- kisi select
+-- select statement
+describe actor;
+
+desc actor;
+
+-- dql statement
+select * from actor;
+
+select actor_id from actor;
+
+
+select actor_id,first_name from actor;
+
+DESC ACTOR_INFO;
+
+-- PROJECTION AND SELECTION
+
+SELECT * FROM ACTOR_INFO;
+
+select ACTOR_ID,ACTOR_ID*10 FROM ACTOR_INFO;
+
+select * FROM ACTOR_INFO;
+
+-- WHERE CLAUSE
+SELECT * FROM ACTOR_INFO WHERE ACTOR_ID=2;
+
+
+SELECT *
+ FROM ACTOR_INFO WHERE FIRST_NAME="ed";
+ 
+ 
+ 
+ -- statement vs cluase
+ -- where cluase,internal
+ -- mysql install + sakila + world database
+ -- dbms  vs  rdbms
+ -- acid properties
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
